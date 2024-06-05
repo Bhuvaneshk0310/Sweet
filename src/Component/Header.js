7472f157374406106244539e1c746666849e03e0
Bhuvaneshk0310 <bhuvaneshk0310@gmail.com>
2024-06-05 16:05:41 +0530

    sweets projects

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,63 +1,57 @@
 import React, { useContext } from "react";
 import './Header.css';
 import { Link } from 'react-router-dom';
-import * as image from "../Component/ImagesFolder/Image"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import * as image from "../Component/ImagesFolder/Image";
 import { CartContext } from './CartContext';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';  // Import Bootstrap JS bundle
+import 'font-awesome/css/font-awesome.min.css';
 
 const Header = () => {
   const { cart } = useContext(CartContext);
-
   const cartCount = cart.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <div>
-      <nav class="navbar navbar-dark navbar-expand-md fixed-top">
-        <div class="container">
-        <span><img src={image.s} alt="" width="30" height="24" class="d-inline-block align-text-top s1" /></span>
-          <a class="navbar-brand" href="/Home">Sweets</a>
-          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-            data-target="#navbarNav">
-            <span class="navbar-toggler-icon "></span>
+      <nav className="navbar navbar-dark navbar-expand-md fixed-top">
+        <div className="container">
+          <span>
+            <img src={image.s} alt="" width="30" height="24" className="d-inline-block align-text-top s1" />
+          </span>
+          <a className="navbar-brand" href="/Home">Sweets</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-              <Link to="/Home">
-                <li class="nav-item">
-                  <a class="nav-link y1" >Home</a>
-                </li></Link>
-              <Link to="/Menu">
-                <li class="nav-item">
-                  <a class="nav-link y1" >Menu</a>
-                </li></Link>
-              <Link to="/Contact">
-                <li class="nav-item">
-                  <a class="nav-link y1" >Contact </a>
-                </li></Link>
-              <Link to="/Searchs">
-                <li class="nav-item">
-                  <a class="nav-link y1" >Search</a>
-                </li>
-              </Link>
-              <Link to="/Cart">
-                <li class="nav-item">
-                  <div class="shopping">
-                    <img src={image.cart} />
-                    <span className=" count">{cartCount}</span>
-                    <a class="nav-link shop1" >Cart</a>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to="/Home" className="nav-link y1">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/Menu" className="nav-link y1">Menu</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/Contact" className="nav-link y1">Contact</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/Searchs" className="nav-link y1">Search</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/Cart" className="nav-link">
+                  <div className="shopping">
+                    <img src={image.cart} alt="Cart" />
+                    <span className="count">{cartCount}</span>
+                    <a className="shop1">Cart</a>
                   </div>
-                </li>
-              </Link>
-                      </ul>
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
-
-   
- 
-
+      
     </div>
-  )
-}
+  );
+};
 
 export default Header;
--- src/Component/Searchs.js
+++ src/Component/Searchs.js
@@ -7,7 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Footer from './Footer';
 
-
 function Searchs() {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredData, setFilteredData] = useState([]);
@@ -53,43 +52,145 @@ function Searchs() {
 
   return (
     <div>
-      <div className="container p-5 my-5 ">
+      <div className="container p-5 my-5">
         <div className="searchInput_Container">
-        <input
+          <input
             id="searchInput"
             type="text"
             placeholder="Search something Sweet..."
             value={searchTerm}
-            onChange={handleSearch}/>
-            <img className="sea1" src={image.searchs} alt="search" />
-          
-        
+            onChange={handleSearch}
+          />
+          <img className="sea1" src={image.searchs} alt="search" />
         </div>
-        <div className="container-fluid ">
-          {filteredData.map((val) => (
-            <div className="template" key={val.id}>
-              <FavoriteButton
-                isFavorite={val.isFavorite} 
-                id={val.id} 
-              />
-              <div>
-                <Link to={`/Searchdetail/${val.id}`}>
-                  <img className="sweetimg" src={val.image} alt={val.title} />
-                  <div className="tp2">
-                    <p className=" title1">{val.title}</p>
-                    <p className=" price1">₹{val.price}</p>
+        <div className="container">
+          <div className="row">
+            {filteredData.map((val) => (
+              <div className="col-md-4" key={val.id}>
+                <div className="template rounded border mt-5">
+                  <FavoriteButton isFavorite={val.isFavorite} id={val.id} />
+                  <div>
+                    <Link to={`/Searchdetail/${val.id}`}>
+                      <img
+                        className="sweetimg img-fluid" // Apply Bootstrap's img-fluid class here
+                        src={val.image}
+                        alt={val.title}
+                      />
+                      <div className="tp2">
+                        <p className="title1">{val.title}</p>
+                        <p className="price1">₹{val.price}</p>
+                      </div>
+                    </Link>
                   </div>
-                </Link>
+                </div>
               </div>
-             
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
 
 export default Searchs;
+
+
+
+
+
+// import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+// import "./Searchs.css";
+// import data from "./TemplateData.json";
+// import * as image from "../Component/ImagesFolder/Image";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import Footer from './Footer';
+
+
+// function Searchs() {
+//   const [searchTerm, setSearchTerm] = useState('');
+//   const [filteredData, setFilteredData] = useState([]);
+
+//   useEffect(() => {
+//     setFilteredData(data);
+//   }, []);
+
+//   // Update the filtered data whenever the search term changes
+//   useEffect(() => {
+//     const filtered = data.filter((val) =>
+//       val.title.toLowerCase().includes(searchTerm.toLowerCase())
+//     );
+//     setFilteredData(filtered);
+//   }, [searchTerm]);
+
+//   const handleSearch = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   const handleFavorite = (id) => {
+//     // Find the item by ID in your data array
+//     const item = filteredData.find((item) => item.id === id);
+//     // Toggle the favorite state of the item
+//     item.isFavorite = !item.isFavorite;
+//     // Update the filtered data with the new favorite state
+//     setFilteredData([...filteredData]);
+//   };
+
+//   const FavoriteButton = ({ isFavorite, id }) => {
+//     const handleClick = () => {
+//       handleFavorite(id); // Handle favorite logic
+//     };
+
+//     return (
+//       <div>
+//         <button className='heart' onClick={handleClick}>
+//           {isFavorite ? '❤️' : '🤍'}
+//         </button>
+//       </div>
+//     );
+//   };
+
+//   return (
+//     <div>
+//       <div className="container p-5 my-5 ">
+//         <div className="searchInput_Container">
+//         <input
+//             id="searchInput"
+//             type="text"
+//             placeholder="Search something Sweet..."
+//             value={searchTerm}
+//             onChange={handleSearch}/>
+//             <img className="sea1" src={image.searchs} alt="search" />
+          
+        
+//         </div>
+//         <div className="container-fluid ">
+//           {filteredData.map((val) => (
+//             <div className="template" key={val.id}>
+//               <FavoriteButton
+//                 isFavorite={val.isFavorite} 
+//                 id={val.id} 
+//               />
+//               <div>
+//                 <Link to={`/Searchdetail/${val.id}`}>
+//                   <img className="sweetimg" src={val.image} alt={val.title} />
+//                   <div className="tp2">
+//                     <p className=" title1">{val.title}</p>
+//                     <p className=" price1">₹{val.price}</p>
+//                   </div>
+//                 </Link>
+//               </div>
+             
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//       <Footer/>
+//     </div>
+//   );
+// }
+
+// export default Searchs;
 
